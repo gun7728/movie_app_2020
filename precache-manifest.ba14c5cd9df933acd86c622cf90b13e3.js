e78dce37d4e3bd2aab344f0d6c01a98d7dcd70b0
gun7728 <gun7728@gmail.com>
2020-04-23 01:21:47 +0900

    Updates

---
+++ precache-manifest.ba14c5cd9df933acd86c622cf90b13e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b13839f981edf6ad3643a5e3a02702",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1bb29f8691b522710380",
+    "url": "/movie_app_2020/static/css/main.5591e965.chunk.css"
+  },
+  {
+    "revision": "08f6efee45481d1ea76e",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb29f8691b522710380",
+    "url": "/movie_app_2020/static/js/main.2244be12.chunk.js"
+  },
+  {
+    "revision": "e76fc168c1d893da3cab",
+    "url": "/movie_app_2020/static/js/runtime-main.ea46b0e5.js"
+  }
+]);
